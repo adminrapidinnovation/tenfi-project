5cf495f9eb4c4d871847766481905be8969e8509
ayrapidinnovation <ay@rapidinnovation.dev>
2021-05-04 19:12:29 +0530

    changes

--- components/Assets/AssetList.js
+++ components/Assets/AssetList.js
@@ -3,59 +3,33 @@ import { useSelector } from "react-redux";
 import ChevronDownIcon from "icons/ChevronDownIcon";
 import AssetsListDropdown from "./AssetListDropdown";
 import styles from "styles/modules/Assets/AssetList.module.scss";
-import BUSDIcon from "assets/icons/busd.svg";
-import USDTIcon from "assets/icons/USDT.png";
-import FourBeltIcon from "assets/icons/4belt-icon.png";
-import AQUAIcon from "assets/images/TEnfi-token.svg";
-import BNBIcon from "assets/icons/binance-chai.svg";
-import VAIIcon from "assets/icons/VAI.png";
-import USTIcon from "assets/icons/UST.png";
+import TENFiIcon from "assets/images/TEnfi-token.svg";
+import BNBIcon from "assets/images/binance-coin-bnb-logo.svg";
+import BUSDIcon from "assets/images/busd.svg";
+import USDCIcon from "assets/images/usdc.8dd9681c.svg";
 import AssetIcons from "./AssetIcons";
 
 const renderIcon = (rowId, token) => {
   const title = token.split("-");
   if (rowId === 0) {
+    return <AssetIcons firstImg={TENFiIcon} firstTitle={title[0]} />;
+  }
+  if (rowId === 1) {
     return (
       <AssetIcons
-        firstImg={AQUAIcon}
-        firstTitle={"tenfi"}
+        firstImg={TENFiIcon}
+        firstTitle={title[0]}
         secondImg={BNBIcon}
         secondTitle={title[1]}
       />
     );
   }
-  if (rowId === 1) {
-    return <AssetIcons firstImg={AQUAIcon} firstTitle={"tenfi"} />;
-  }
   if (rowId === 2) {
-    return <AssetIcons firstImg={FourBeltIcon} firstTitle={title[0]} />;
-  }
-  if (rowId === 3) {
     return (
       <AssetIcons
         firstImg={BUSDIcon}
         firstTitle={title[0]}
-        secondImg={USDTIcon}
-        secondTitle={title[1]}
-      />
-    );
-  }
-  if (rowId === 4) {
-    return (
-      <AssetIcons
-        firstImg={BUSDIcon}
-        firstTitle={title[0]}
-        secondImg={VAIIcon}
-        secondTitle={title[1]}
-      />
-    );
-  }
-  if (rowId === 5) {
-    return (
-      <AssetIcons
-        firstImg={BUSDIcon}
-        firstTitle={title[0]}
-        secondImg={USTIcon}
+        secondImg={USDCIcon}
         secondTitle={title[1]}
       />
     );
@@ -67,8 +41,6 @@ function AssetList(props) {
   const { poolData, poolDataLoading } = selector.user;
   const [activeIndex, setActiveIndex] = useState(-1);
 
-  console.log("selector-->", selector);
-
   function toggleActive(index) {
     setActiveIndex(index === activeIndex ? -1 : index);
   }
@@ -78,7 +50,6 @@ function AssetList(props) {
       if (poolData[index] !== undefined) {
         return (
           <Fragment key={index}>
-            {console.log("item==>", item)}
             <tr
               className={`${styles.headrow} ${
                 index === activeIndex ? styles.active : ""
@@ -88,7 +59,10 @@ function AssetList(props) {
               <td className={styles.colEmpty}>&nbsp;</td>
               <td className={styles.colApi}>
                 <span className={styles.badge}>
-                  APY {parseFloat(item.totalApy).toFixed(2)}
+                  APY{" "}
+                  {item.totalApy
+                    ? parseFloat(item.totalApy).toFixed(2)
+                    : "0.00"}
                 </span>
               </td>
               <td className={styles.colText1}>
